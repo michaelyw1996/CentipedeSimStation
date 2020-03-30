4694dfb9e7c4542045fa69f491aabe8acc86fed4
michaelyw1996 <44041891+michaelyw1996@users.noreply.github.com>
2020-03-30 15:25:06 -0700

    MVC

---
+++ mvc/View.java
@@ -0,0 +1,38 @@
+package mvc;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class View extends JComponent implements PropertyChangeListener {
+    protected Model model;
+
+    public View(Model model) {
+        super();
+        this.model = model;
+        model.addPropertyChangeListener(this);
+		setBorder(LineBorder.createBlackLineBorder());
+
+    }
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model model) {
+        if(this.model != null) {
+            this.model.removePropertyChangeListener(this);
+        }
+        this.model = model;
+        if (model != null) {
+            this.model.addPropertyChangeListener(this);
+            repaint();
+        }
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+        repaint();
+    }
+}
