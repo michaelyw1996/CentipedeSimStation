722f3d64a758d4a7080cb3060ec7611cde97077b
stevenwcliu <56695863+stevenwcliu@users.noreply.github.com>
2020-04-14 16:58:24 -0700

    Updated PDT populate()

--- SimStation/prisoners/PDTournament.java
+++ SimStation/prisoners/PDTournament.java
@@ -1,7 +1,9 @@
 /*
-* Wencong Liu, 4/14: 
+* Wencong Liu, 4/14:
 * Vyvy Tran, 4/14: Added a few missing functions
 */
+package prisoners;
+import mvc.*;
 
 class PDFactory extends SimulationFactory{
   public Model makeModel() { return new PDSimulation();}
@@ -53,36 +55,36 @@ class Prisoner extends Agent{
   public synchronized boolean getLastResponse(){
      return lastResponse;
   }
-    
+
   public synchronized void setLastResponse(boolean lastResponse){
      this.lastResponse = lastResponse;
   }
-	
+
   public CooperateStrategy getStrategy(){
      return strategy;
   }
-	
+
   public synchronized boolean wasCheated(){
      return lastResponse;
   }
-    
+
    public synchronized void setWasCheated(boolean wasCheated){
      this.lastResponse = wasCheated;
   }
-	
+
    public void setStrategy(CooperateStrategy strategy){
      this.strategy = strategy;
      strategy.setOwner(this);
    }
-	
+
    public int getFitness(){
      return fitness;
    }
-	
+
    public synchronized boolean cooperate(){
      return strategy.cooperate();
    }
-    
+
    public synchronized void updateFitness(boolean myChoice, boolean nbrChoice){
 	  if (myChoice){
 			if (nbrChoice){ fitness += 3; }
@@ -92,19 +94,39 @@ class Prisoner extends Agent{
 			else { fitness += 1; }
 		}
 	 }
-    
+
    public void update() {
 	 Prisoner neighbor = (Prisoner)world.getNeighbor(asker, radius);
-		
+
 	 if (neighbor != null) {
 		 boolean myChoice = cooperate();
 		 lastResponse = neighbor.cooperate();
 		 updateFitness(myChoice, lastResponse);
 		 neighbor.updateFitness(lastResponse, myChoice);
-	}		
+	}
 	setHeading(Heading.random());
         move(10);
    }
-  
+
   }
+
+public class PDTournament extends Simulation{
+  public static int numAgents = 10;
+
+  public void populate(){
+    super.populate();
+    for(int i =0; i < numAgents; i++){
+      this.addAgent(new Prisoner(new AlwaysCheat()));
+    }
+    for(int i =0; i < numAgents; i++){
+      this.addAgent(new Prisoner(new AlwaysCooperate()));
+    }
+    for(int i =0; i < numAgents; i++){
+      this.addAgent(new Prisoner(new RandomlyCheat()));
+    }
+    for(int i =0; i < numAgents; i++){
+      this.addAgent(new Prisoner(new Reciprocate()));
+    }
+  }
+}
 }
