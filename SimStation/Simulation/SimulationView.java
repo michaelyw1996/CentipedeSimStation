666c933d8e4a6029c52b9b15d3cae7148845e811
stevenwcliu <wencong.liu@sjsu.edu>
2020-04-14 22:57:45 -0700

    updated SimulationView
    updated the paintComponent()

--- SimStation/Simulation/SimulationView.java
+++ SimStation/Simulation/SimulationView.java
@@ -7,7 +7,6 @@ package SimStation;
 
 import java.awt.Graphics;
 import java.util.Iterator;
-
 import mvc.*;
 
 public class SimulationView extends View{
@@ -15,15 +14,27 @@ public class SimulationView extends View{
 	public SimulationView(Model model) {
 		super(model);
 	}
-	
-   	public void paintComponent(Graphics gc)
-    	{
-	   	Simulation simulation = (Simulation)model;    	        
-	     
-	    	for (Agent a: ((Simulation)model).getAgents())
-	    	{
-	     	   gc.setColor(Color.RED);
-	     	   gc.fillOval(a.getXc(), a.getYc(), 5, 5);
-	    	}
-   	 }
+
+   	//public void paintComponent(Graphics gc)
+    	//{
+	   	// Simulation simulation = (Simulation)model;
+			//
+	    // 	for (Agent a: ((Simulation)model).getAgents())
+	    // 	{
+	    //  	   gc.setColor(Color.RED);
+	    //  	   gc.fillOval(a.getXc(), a.getYc(), 5, 5);
+	    // 	}
+
+			public void paintComponent(Graphics gc)
+			{
+				Color oldColor = gc.getColor();
+				gc.setColor(Color.RED);
+				Simulation sim = (Simulation)model;
+				Iterator<Agent> it = sim.iterator();
+				while(it.hasNext()) {
+					Agent a= it.next();
+					gc.fillOval(a.getXc(), a.getYc(), 5, 5);
+				}
+				gc.setColor(oldColor);
+   	 	}
 }
