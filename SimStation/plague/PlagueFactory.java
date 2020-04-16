a5a9a79c892c560ffb83119529a8e1d4537e49fc
vyvtran <62912798+vyvtran@users.noreply.github.com>
2020-04-15 22:46:34 -0700

    Update PlagueFactory.java

--- SimStation/plague/PlagueFactory.java
+++ SimStation/plague/PlagueFactory.java
@@ -6,3 +6,7 @@ import mvc.Model;
 public class PlagueFactory extends SimulationFactory{
 	public Model makeModel(){ return new PlagueSimulation();}
 }
+
+public View getView(Model model) {
+		return new PlagueView((PlagueSimulation) model);
+}
